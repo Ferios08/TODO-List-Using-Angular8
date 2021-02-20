aae2c300b8ea7c50f66f3b97dca0d581c553cdee
Firas Chbiki <firas.chbiki@sofrecom.com>
2021-02-20 08:59:25 +0100

    fixed and returned to classic env file

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Card } from '../models/card';
+import { CardsService } from '../sercices/card.service';
+
 
 @Component({
   selector: 'app-list',
@@ -8,26 +11,58 @@ import { Component, OnInit } from '@angular/core';
 export class ListComponent implements OnInit {
   public items = [];
   public newTask;
+  public elements = 0;
 
-  constructor() { }
+  constructor(
+    private cardsService: CardsService,
+  ) { }
 
   ngOnInit() {
+    this.getAll();
+    
   }
 
 
-    public addToList() {
+  public addToList() {
     if (this.newTask == '') {
     }
     else {
       this.items.push(this.newTask);
-      this.newTask = '';
+
+      // push to database
+      return new Promise((resolve, reject) => {
+        this.cardsService.add({ "task": this.newTask }).subscribe(data => {
+          this.newTask = '';
+          this.ngOnInit();
+        });
+      });
+
     }
     console.log(this.items)
   }
 
 
   public deleteTask(index) {
-    this.items.splice(index, 1); 
-   }
+    this.cardsService.delete(this.items[index].id).subscribe((response) => {
+      // console.log("deleted");
 
+    });
+    this.ngOnInit();
+  }
+
+
+
+
+  getAll(): any {
+    return new Promise((resolve, reject) => {
+      this.cardsService.getAll().subscribe(data => {
+        resolve(data);
+        this.items = data as Card[];
+        console.log(data);
+        this.elements = Object.keys(data).length;
+        console.log("You have " + this.elements + " tasks :)");
+        
+      });
+    });
+  }
 }
